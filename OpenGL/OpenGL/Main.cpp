702409eb1e2fc36659a0a186402f3a8a4b8c55a5
danial1360 <danial1360@hotmail.com>
2016-04-26 10:46:38 +0100

    done

--- OpenGL/OpenGL/Main.cpp
+++ OpenGL/OpenGL/Main.cpp
@@ -1,4 +1,30 @@
 #define GLEW_STATIC
 #include <GL\glew.h>
-#include <GLFW\glfw3.h>
- jj
+#include<GLFW\glfw3.h>
+#include <iostream>
+void main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+	GLFWwindow * m_window = glfwCreateWindow(800, 600, "non", nullptr, nullptr);
+		if (m_window == nullptr)
+		{
+			return ;
+		}
+		glfwMakeContextCurrent(m_window);
+		glewExperimental = GL_TRUE;
+		if (glewInit() != GLEW_OK)
+		{
+			return;
+		}
+		glViewport(0, 0, 200, 200);
+		while (!glfwWindowShouldClose(m_window))
+		{
+			glfwPollEvents();
+			glfwSwapBuffers(m_window);
+		}
+		glfwTerminate();
+}
